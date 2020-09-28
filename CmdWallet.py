7b051b2312f07a469ae267661f7cacff6791e229
DogeRock <71775028+DogeRock@users.noreply.github.com>
2020-09-28 11:45:47 -0400

    Update CmdWallet.py

--- CmdWallet.py
+++ CmdWallet.py
@@ -21,10 +21,22 @@ def handler(signal_received, frame):
 
 signal(SIGINT, handler)
 
-print("Welcome to the DogeRock Duino-Coin cmd line wallet\n")
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+print(color.YELLOW + "Welcome to the DogeRock Duino-Coin cmd line wallet\n" + color.END)
 
 def registerprompt():
-  register = input("Would you like to register? (yes/no)\n>")
+  register = input(color.BLUE + "Would you like to register? (yes/no)\n>" + color.END)
   return register
 
 registerInput = registerprompt()
@@ -38,7 +50,7 @@ if (registerInput == "yes" or registerInput == "y"):
     regiFeedback = soc.recv(256).decode().split(",")
 
     if regiFeedback[0] == "OK":
-      print("Successfully registered new account!")
+      print(color.CYAN + "Successfully registered new account!" + color.END)
       break
     elif regiFeedback[0] == "NO":
       print("Cannot make account! Reason: " + str(regiFeedback[1]) + "\nExiting in 10s")
@@ -46,58 +58,62 @@ if (registerInput == "yes" or registerInput == "y"):
       os._exit(1)
 
 def login():
-  username = input("\nPlease enter your username:\n>")
-  password = input("\nPlease enter your password:\n>")
+  username = input(color.GREEN + "\nPlease enter your username:\n>" + color.END)
+  password = input(color.GREEN + "\nPlease enter your password:\n>" + color.END)
 
   soc.send(bytes("LOGI," + username + "," + password, encoding="utf8"))
   response = soc.recv(2).decode()           
   if response != "OK":
-    print("Invalid password! Exiting in 10s\n")
+    print(color.RED + "Invalid password! Exiting in 10s\n" + color.END)
     time.sleep(10)
     os._exit(1)
   else:
-    print("\nSuccessfully logged in!")
+    print(color.CYAN + "\nSuccessfully logged in!" + color.END)
     soc.send(bytes("FROM,DogeRockWallet,"+str(username), encoding="utf8"))
-  print("\nWelcome " + username)
-  print("\nFor a list of commands type: help\n")
+  print(color.CYAN + "\nWelcome " + username + color.END)
+  print(color.CYAN + "\nFor a list of commands type: help\n" + color.END)
 
 login()
 
 def command():
-  command1 = input(">")
+  command1 = input(color.DARKCYAN + ">" + color.END)
   if command1 == "help":
-    print("Help command: help\nBalance command: balance\nSend command: send\nChange password: changepassword")
+    print(color.DARKCYAN + "Help command: help\nBalance command: balance\nSend command: send\nChange password: changepassword" + color.END)
     command()
   elif command1 == "balance":
     soc.send(bytes("BALA", encoding="utf8"))
     balance = soc.recv(1024).decode()
 
-    print("Duino balance: " + balance)
+    print(color.BLUE + "Duino balance: " + balance + color.END)
     command()
   elif command1 == "send":
-    recipient = input("Enter recipients' username: ")
-    amount = input("Enter amount to transfer: ")
-    soc.send(bytes("SEND,deprecated,"+str(recipient)+","+str(amount), encoding="utf8"))
-    while True:
-      message = soc.recv(1024).decode()
-      print(str(message))
+    recipient = input(color.YELLOW + "Enter recipients' username: " + color.END)
+    amount = input(color.YELLOW + "Enter amount to transfer: " + color.END)
+    confirm = input(color.YELLOW + "Type" + color.BOLD + " confirm " + color.END + color.YELLOW + "to confirm\n>" + color.END)
+    if confirm == "confirm":
+      soc.send(bytes("SEND,deprecated,"+str(recipient)+","+str(amount), encoding="utf8"))
+      while True:
+        message = soc.recv(1024).decode()
+        print(color.YELLOW + str(message) + color.END)
+        command()
+        break
+    else:
+      print(color.RED + "Command cancled" + color.END)
       command()
-      break
   elif command1 == "exit":
     soc.close()
     os._exit(1)
   elif command1 == "changepassword":
-    oldpassword = input("Enter your current password\n>")
-    newpassword = input("Enter new password\n>")
+    oldpassword = input(color.GREEN + "Enter your current password\n>" + color.END)
+    newpassword = input(color.GREEN + "Enter new password\n>" + color.END)
     soc.send(bytes("CHGP,"+  str(oldpassword) + "," + str(newpassword), encoding="utf8"))
     while True:
       message = soc.recv(1024).decode()
-      print(str(message))
+      print(color.GREEN + str(message) + color.END)
       command()
       break
   else:
-    print("Unknown command!")
-    time.sleep(2)
+    print(color.RED + "Unknown command!" + color.END)
     command()
 
 command()
